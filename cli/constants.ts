db03f47491c0ebfcf06a3486dc7999a511f1f743
pavan-c247 <pavan.s@chapter247.com>
2026-01-15 19:09:33 +0530

    chore: update constant

--- cli/constants.ts
+++ cli/constants.ts
@@ -1,2 +1,2 @@
 export const BOILERPLATE_REPO = "https://github.com/Chapter247IND/Frontend-theme-2025.git";
-export const BOILERPLATE_BRANCH = "feat/module-selection-cli-config";
+export const BOILERPLATE_BRANCH = "development";
